e96f6c4ae30fccb0ab56b33a68926d64f4090195
Warren A. Kibbe <wakibbe@northwestern.edu>
2012-08-16 17:26:04 -0500

    adding changed location for format_helper and latticegrid to work with Ruby 1.9.3

---
+++ lib/latticegrid.rb
@@ -0,0 +1,38 @@
+module LatticeGrid
+  def LatticeGrid.the_instance
+    if "#{File.expand_path(RAILS_ROOT)}" =~ /Users/
+      'RHLCCC'
+    else
+      case "#{File.expand_path(RAILS_ROOT)}"
+        when /fsm/i
+          'Feinberg'
+        when /cancer/i
+          'RHLCCC'
+        when /rhlccc/i
+          'RHLCCC'
+        when /ccne/i
+          'CCNE'
+        when /lls/i
+          'LLS'
+        when /umich/i
+          'UMich'
+        when /uwisc/i
+          'UWCCC'
+        when /stanford/i
+          'Stanford'
+        when /ucsf/i
+          'UCSF'
+        when /cinj/i
+          'CINJ'
+        when /uchicago/i
+          'UChicago'
+        when /uic/i
+          'UIC'
+        when /aas/i
+          'AAS'
+        else
+          'defaults'
+      end
+    end
+  end
+end
