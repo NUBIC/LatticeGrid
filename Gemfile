a8d26b6f594f50aa1206b105ad00a8d273f912f1
Paul Friedman <p-friedman@northwestern.edu>
2013-11-12 15:00:29 -0600

    First step in Rails upgrade

--- Gemfile
+++ Gemfile
@@ -2,29 +2,65 @@ source 'https://rubygems.org'
 source 'http://download.bioinformatics.northwestern.edu/gems'
 
 gem 'bundler'
-gem "rails", "~> 2.3", :require => nil
+
+gem 'rails', '3.1.10'
+
 gem 'rmagick', :require => nil
 gem 'ruby-graphviz'
-gem 'json', "1.6.6"
+gem 'json'
+
 #gem 'sparklines'
 gem 'fastercsv'
 gem 'pg'
-gem 'haml', '3.1.7'
-gem 'will_paginate', "~> 2.3"
-gem 'awesome_nested_set', "~> 1.4"
+gem 'haml'
+gem 'will_paginate'
+gem 'awesome_nested_set'
 #gem "acts_as_taggable_on_steroids", "~> 1.2"
-gem "princely", "~> 1.2.5"
+gem 'princely'
 gem 'net-ldap'
 gem 'bio'
 gem 'aker-rails'
 gem 'aker'
-gem 'rdoc', '3.12' # necessary for rails 2.3.12 and higher
-gem 'bcdatabase', '~> 1.0.6'
-#gem 'wordcram'
+gem 'rdoc'
+gem 'bcdatabase'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyracer'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# To use debugger
+# gem 'ruby-debug'
 
 group :development do
   gem 'capistrano'
   gem 'capistrano-bundler'
   gem 'capistrano-rails'
   gem 'annotate', '~> 2.5.0'
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'poltergeist'
+  gem 'database_cleaner'
+  gem 'shoulda'
+  gem 'factory_girl'
+  gem 'simplecov', require: false
+  gem 'fakeweb'
+  gem 'webmock', require: false
+  gem 'rack-test'
 end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,54 +1,60 @@
-require 'latticegrid'
+LatticeGrid::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
 
-# Settings specified here will take precedence over those in config/environment.rb
+  # Code is not reloaded between requests
+  config.cache_classes = true
 
-# In the development environment your application's code is reloaded on
-# every request.  This slows down response time but is perfect for development
-# since you don't have to restart the webserver when you make code changes.
-config.cache_classes = false
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
-# Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
 
-# Enable the breakpoint server that script/breakpointer connects to
-#config.breakpoint_server = true
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
 
-# Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
-config.action_view.debug_rjs                         = true
-config.action_controller.perform_caching             = true
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
 
-the_relative_url_root = case LatticeGrid.the_instance
-  when 'RHLCCC' then "cancer-latticegrid"
-  when 'Feinberg' then "fsm-latticegrid"
-  when 'Stanford' then "stanford-latticegrid"
-  when 'CCNE' then "ccne-latticegrid"
-  when 'UCSF' then "ucsf-latticegrid"
-  when 'UWCCC' then "uwisc-latticegrid"
-  when 'UMich' then "umich-latticegrid"
-  when 'CINJ' then "cinj-latticegrid"
-  when 'UChicago' then "uchicago-latticegrid"
-  when 'UIC' then "uic-latticegrid"
-  when 'AAS' then "aas-latticegrid"
-  when 'LLS' then "lls-latticegrid"
-  else "latticegrid"
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
+  # Defaults to Rails.root.join("public/assets")
+  # config.assets.manifest = YOUR_PATH
+
+  # Specifies the header that your server uses for sending files
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
+
+  # See everything in the log (default is :info)
+  # config.log_level = :debug
+
+  # Use a different logger for distributed setups
+  # config.logger = SyslogLogger.new
+
+  # Use a different cache store in production
+  # config.cache_store = :mem_cache_store
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server
+  # config.action_controller.asset_host = "http://assets.example.com"
+
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  # config.assets.precompile += %w( search.js )
+
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable threaded mode
+  # config.threadsafe!
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
 end
-
-config.action_controller.relative_url_root           = "/#{the_relative_url_root}" if ENV["HOME"] =~ /home/
-
-# Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = false
-
-config.after_initialize do
-  Aker.configure do
-    if RAILS_ROOT =~ /Users/
-      login_config = File.join(RAILS_ROOT, %w(config logins development.yml))
-      authority Aker::Authorities::Static.from_file(login_config)
-      puts "loading local static aker file"
-    else
-      authorities :ldap
-      central '/etc/nubic/bcsec-staging.yml'
-    end
-  end
-end
-
