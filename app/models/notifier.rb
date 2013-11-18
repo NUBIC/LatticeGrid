d4ed4011c649e40405e0bd26fd5eeb7074af6bf0
Paul Friedman <p-friedman@northwestern.edu>
2013-11-18 14:40:09 -0600

    Removed deprecated attribute #4648
    Deprecated ActionMailer attribute :sent_on
    Using the new mailer API, you can specify :date to the mail method.
    More information:
    http://stackoverflow.com/questions/7367185/weird-error-when-delivering-mail-undefined-method-index-for-2011-09-09-2215
    
    The culprits:
    	- app/models/notifier.rb

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,21 +1,20 @@
 class Notifier < ActionMailer::Base
-  
+
   def reminder_message(last_name, to, from, subject, profile_url, login_url, edit_publications_url, edit_profile_url, pub_total, pub_valid, abstract)
      @recipients   = to
      @from         = from
      headers         "Reply-to" => "#{from}"
      @subject      = subject
-     @sent_on      = Time.now
      @content_type = "text/html"
 
      body[:last_name]  = last_name
-     body[:profile_url] = profile_url       
-     body[:login_url] = login_url       
-     body[:edit_publications_url] = edit_publications_url       
-     body[:edit_profile_url] = edit_profile_url       
-     body[:pub_total] = pub_total       
-     body[:pub_valid] = pub_valid       
-     body[:abstract] = abstract       
+     body[:profile_url] = profile_url
+     body[:login_url] = login_url
+     body[:edit_publications_url] = edit_publications_url
+     body[:edit_profile_url] = edit_profile_url
+     body[:pub_total] = pub_total
+     body[:pub_valid] = pub_valid
+     body[:abstract] = abstract
    end
 
 end
