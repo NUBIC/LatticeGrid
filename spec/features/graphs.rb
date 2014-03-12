376b77a0caa21dd6cfc1dd50b29c6a958a173a88
Paul Friedman <p-friedman@northwestern.edu>
2014-03-12 17:35:46 -0500

    wip: placeholder for feature re: graphs

---
+++ spec/features/graphs.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe 'Graphs' do
+
+  # GET /graphs/:organizational_unit_id/show_org
+
+  # GET /graphviz/:organizational_unit_id/show_org
+
+  # GET /graphviz/:organizational_unit_id/show_org_mesh
+
+  # GET /graphviz/:organizational_unit_id/show_org_org
+
+  # GET /cytoscape/:organizational_unit_id/program_chord
+
+  # GET /cytoscape/chord
+
+  # GET /cytoscape/:organizational_unit_id/show_org
+
+  # GET /cytoscape/:organizational_unit_id/awards_org
+
+  # GET /cytoscape/:organizational_unit_id/studies_org
+
+  # GET /cytoscape/:organizational_unit_id/org_all
+end
