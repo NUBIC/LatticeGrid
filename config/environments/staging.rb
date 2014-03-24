4628bdc55980623c90b3b0395c0f5cd03f513e65
Paul Friedman <p-friedman@northwestern.edu>
2014-03-24 15:59:48 -0500

    fall back to asset pipeline on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -15,7 +15,7 @@ LatticeGrid::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true # false - Staging server not finding pre-compiled assets so falling back to asset pipeline
 
   # Generate digests for assets URLs
   config.assets.digest = true
