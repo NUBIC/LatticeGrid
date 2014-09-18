98c4377921aeab3e391ef43c9f5322597132592c
Paul Friedman <p-friedman@northwestern.edu>
2014-09-18 15:44:34 -0500

    added specs for d3_generator

--- lib/d3_generator.rb
+++ lib/d3_generator.rb
@@ -1,12 +1,11 @@
 # sample d3 json structure:
-#[{:name=>'flash', :size=>400, :imports=>["flash","javascript", "d3", "cytoscape"]},
-# {:name=>'javascript', :size=>8000, :imports=>["d3", "cytoscape","javascript"]},
-# {:name=>'d3', :size=>800, :imports=>["d3"]},
-# {:name=>'cytoscape', :size=>400, :imports=>["cytoscape","javascript", "d3", "cytoscape"]}]
+#[{name: 'flash', size: 400, imports: ["flash","javascript", "d3", "cytoscape"]},
+# {name: 'javascript', size: 8000, imports: ["d3", "cytoscape","javascript"]},
+# {name: 'd3', size: 800, imports: ["d3"]},
+# {name: 'cytoscape', size: 400, imports: ["cytoscape","javascript", "d3", "cytoscape"]}]
 
 def d3_units_by_date_graph(units, start_date, end_date)
   graph_array = []
-  investigator_array = []
   units.each do |unit|
     faculty_ids = unit.primary_or_member_faculty.map(&:id)
     next if faculty_ids.blank?
@@ -15,12 +14,11 @@ def d3_units_by_date_graph(units, start_date, end_date)
       graph_array << d3_unit_investigator_by_date_graph(unit, investigator, faculty_ids, start_date, end_date)
     end
   end
-  return graph_array
+  graph_array
 end
 
 def d3_all_units_graph(units)
   graph_array = []
-  investigator_array = []
   units.each do |unit|
     faculty_ids = unit.primary_or_member_faculty.map(&:id)
     next if faculty_ids.blank?
@@ -29,41 +27,37 @@ def d3_all_units_graph(units)
       graph_array << d3_unit_investigator_graph(unit, investigator, faculty_ids)
     end
   end
-  return graph_array
+  graph_array
 end
 
-def d3_all_investigators_graph(program=nil)
+def d3_all_investigators_graph(program = nil)
   if program.blank?
     investigators = Investigator.by_name
   else
     investigators = program.all_primary_or_member_faculty.sort_by(&:last_name)
   end
-  if investigators.blank?
-    return ['']
-  end
+  return [''] if investigators.blank?
   graph_array = []
   coauthor_ids = investigators.map(&:id)
   investigators.each do |master_investigator|
     graph_array << d3_investigator_graph(master_investigator, coauthor_ids, master_investigator.last_name)
   end
-  return graph_array
+  graph_array
 end
 
 def d3_master_investigator_graph(investigator)
-  if investigator.blank?
-    return ['']
-  end
+  return [''] if investigator.blank?
   coauthor_ids = investigator.co_authors.map(&:colleague_id)
-  return add_investigator_to_graph(investigator,coauthor_ids, [])
+  add_investigator_to_graph(investigator, coauthor_ids, [])
 end
 
-def add_investigator_to_graph(investigator,coauthor_ids, graph)
+def add_investigator_to_graph(investigator, coauthor_ids, graph)
   name = investigator.last_name
   graph << d3_investigator_graph(investigator, coauthor_ids, name)
   investigator.colleague_coauthors.each do |colleague|
     graph << d3_investigator_graph(colleague, coauthor_ids, name) if colleague
   end
-  return graph
+  graph
 end
 
 def d3_all_investigators_bundle(investigators)
@@ -77,9 +71,9 @@ def d3_all_investigators_bundle(investigators)
     end
     org_unit = org_unit.blank? ? 'undefined' : org_unit.abbreviation.to_s
     edge = d3_investigator_edge(investigator, org_unit)
-    graph_array << edge unless (edge[:size] == 0)
+    graph_array << edge unless edge[:size] == 0
   end
-  return graph_array
+  graph_array
 end
 
 def d3_master_unit_graph(units, master_unit)
@@ -106,50 +100,50 @@ def d3_master_unit_graph(units, master_unit)
       end
     end
   end
-  return graph_array
+  graph_array
 end
 
 def d3_unit_graph(unit)
   {
-    :name=>unit.abbreviation,
-    :size=>unit.abstracts.count,
-    :imports=>['']
+    name: unit.abbreviation,
+    size: unit.abstracts.count,
+    imports: ['']
   }
 end
 
 def d3_investigator_graph(investigator, coauthor_ids, primary_name)
   {
-    :name => investigator.last_name,
-    :size => investigator.total_publications,
-    :imports => d3_investigator_imports(investigator, coauthor_ids, primary_name)
+    name: investigator.last_name,
+    size: investigator.total_publications,
+    imports: d3_investigator_imports(investigator, coauthor_ids, primary_name)
   }
 end
 
 def d3_investigator_edge(investigator, org_unit)
   first = investigator.first_name.delete("\'")
-  first = first.delete("(")
-  first = first.delete(")")
-  first = first.delete(".")
+  first = first.delete('(')
+  first = first.delete(')')
+  first = first.delete('.')
   {
-    :name => "RHLCCC." +  org_unit + "."  + first  + "-" + investigator.last_name.delete("\'"),
-    :size => investigator.total_publications,
-    :imports => d3_investigator_edge_imports(investigator)
+    name: "RHLCCC.#{org_unit}.#{first}-" + investigator.last_name.delete("\'"),
+    size: investigator.total_publications,
+    imports: d3_investigator_edge_imports(investigator)
   }
 end
 
 def d3_simple_unit_investigator_graph(unit, investigator)
   {
-    :name => unit.abbreviation + "." + investigator.username,
-    :size=>investigator.total_publications,
-    :imports=>['']
+    name: "#{unit.abbreviation}.#{investigator.username}",
+    size: investigator.total_publications,
+    imports: ['']
   }
 end
 
 def d3_unit_investigator_graph(unit, investigator, faculty_ids)
   {
-    :name => unit.abbreviation + "." + investigator.username,
-    :size=>investigator.total_publications,
-    :imports=> d3_unit_investigator_imports(unit, investigator, faculty_ids)
+    name: "#{unit.abbreviation}.#{investigator.username}",
+    size: investigator.total_publications,
+    imports: d3_unit_investigator_imports(unit, investigator, faculty_ids)
   }
 end
 
@@ -158,30 +152,26 @@ def d3_unit_investigator_imports(unit, investigator, faculty_ids)
   return [''] if investigator.co_authors.length < 1
   investigator.co_authors.each do |co_author|
     if faculty_ids.include?(co_author.colleague_id)
-      the_arry << unit.abbreviation + "." + co_author.colleague.username
+      the_arry << "#{unit.abbreviation}.#{co_author.colleague.username}"
     else
       if co_author.colleague && co_author.colleague.appointments.length > 0
-        the_arry << co_author.colleague.appointments[0].abbreviation + "." + co_author.colleague.username
+        the_arry << "#{co_author.colleague.appointments[0].abbreviation}.#{co_author.colleague.username}"
       end
     end
   end
   return [''] if the_arry.blank?
-  return the_arry
+  the_arry
 end
 
 def d3_investigator_imports(investigator, coauthor_ids, primary_name)
   the_arry = []
   return [''] if investigator.colleague_coauthors.length < 1
-  unless investigator.last_name.eql? primary_name
-    the_arry << primary_name
-  end
+  the_arry << primary_name unless investigator.last_name.eql?(primary_name)
   investigator.colleague_coauthors.each do |colleague|
     next unless colleague
-    if coauthor_ids.include?(colleague.id)
-      the_arry << colleague.last_name
-    end
+    the_arry << colleague.last_name if coauthor_ids.include?(colleague.id)
   end
-  return the_arry
+  the_arry
 end
 
 def d3_investigator_edge_imports(investigator)
@@ -201,19 +191,19 @@ def d3_investigator_edge_imports(investigator)
       org_unit = org_unit.abbreviation.to_s
     end
     first = colleague.first_name.delete("\'")
-    first = first.delete("(")
-    first = first.delete(")")
-    first = first.delete(".")
-    the_arry << "RHLCCC." + org_unit.to_s + "." + first + "-" + colleague.last_name.delete("\'")
+    first = first.delete('(')
+    first = first.delete(')')
+    first = first.delete('.')
+    the_arry << "RHLCCC.#{org_unit}.#{first}-" + colleague.last_name.delete("\'")
   end
   the_arry.blank? ? [] : the_arry
 end
 
 def d3_unit_investigator_by_date_graph(unit, investigator, faculty_ids, start_date, end_date)
   {
-    :name => unit.abbreviation + "." + investigator.username,
-    :size => investigator.total_publications,
-    :imports => d3_unit_investigator_by_date_imports(unit, investigator, faculty_ids, start_date, end_date)
+    name:  "#{unit.abbreviation}.#{investigator.username}",
+    size: investigator.total_publications,
+    imports: d3_unit_investigator_by_date_imports(unit, investigator, faculty_ids, start_date, end_date)
   }
 end
 
@@ -222,18 +212,18 @@ def d3_unit_investigator_by_date_imports(unit, investigator, faculty_ids, start_
   return [''] if investigator.co_authors.length < 1
   abstract_ids = investigator.abstracts.abstracts_by_date(start_date, end_date).map(&:id)
   unless abstract_ids.blank?
-    abstract_ids = abstract_ids.join(",").split(",")  # need this to make the ids strings vs an array of integers
+    abstract_ids = abstract_ids.join(',').split(',')  # need this to make the ids strings vs an array of integers
     investigator.co_authors.each do |co_author|
-      incommon_ids =  co_author.publication_list.split(",") & abstract_ids
+      incommon_ids =  co_author.publication_list.split(',') & abstract_ids
       unless incommon_ids.blank?
         if faculty_ids.include?(co_author.colleague_id)
-          the_arry << unit.abbreviation + "." + co_author.colleague.username
+          the_arry << "#{unit.abbreviation}.#{co_author.colleague.username}"
         else
-          the_arry << co_author.colleague.appointments[0].abbreviation + "." + co_author.colleague.username if co_author.colleague.appointments.length > 0 # skip people with no current appointment
+          the_arry << "#{co_author.colleague.appointments[0].abbreviation}.#{co_author.colleague.username}" if co_author.colleague.appointments.length > 0 # skip people with no current appointment
         end
       end
     end
   end
   return [''] if the_arry.blank?
-  return the_arry
+  the_arry
 end
---
+++ spec/lib/d3_generator_spec.rb
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+require 'd3_generator'
+require 'pubmed_utilities'
+require 'publication_utilities'
+require 'mesh_utilities'
+include ApplicationHelper
+
+describe 'd3Generator' do
+
+  let!(:pi) { FactoryGirl.create(:investigator, username: 'pi', first_name: 'Principal', last_name: 'Investigator') }
+  let!(:co) { FactoryGirl.create(:investigator, username: 'co', first_name: 'Co', last_name: 'Author') }
+  let!(:school) { FactoryGirl.create(:school, name: 'school', department_id: '100000', division_id: '100010') }
+  let!(:dept1) { FactoryGirl.create(:department, name: 'dept1', department_id: '101000', division_id: '101010', abbreviation: 'dept1') }
+  let!(:dept2) { FactoryGirl.create(:department, name: 'dept2', department_id: '102000', division_id: '102010', abbreviation: 'dept2') }
+
+  let!(:pi_appt) { FactoryGirl.create(:primary_member, investigator: pi, organizational_unit: dept1) }
+  let!(:co_appt) { FactoryGirl.create(:primary_member, investigator: co, organizational_unit: dept2) }
+
+  let(:abstract_text) { 'Mixed cultures of astrocytes and oligodendrocytes derived from cerebral hemispheres of 18-19 day old rat fetuses were studied with the freeze-fracture technique. The plasma membranes of cultured astrocytes and oligodendrocytes differ substantially in their intramembrane particle profiles, and they can be positively identified consistently. Orthogonal small particle assemblies and numerous isolated globular particles characterize astrocytic plasma membranes, whereas the plasma membranes of oligodendrocytes show numerous elongated particles and fewer large and small globular particles similar to those seen in situ. Using these distinct differential features, we can identify partners of glial cell junctions. We can identify numerous interastrocytic gap junctions, as well as heterologous astrocyte-to-oligodendrocyte gap junctions. The plasma membranes of adjacent oligodendrocytes form numerous tight junctions consisting of linear P face strands and/or rows of particles interrupted by short segments of grooves, the complementary features on the E face. \"Reflexive\" type tight junctions seen in situ are also observed. In addition to intercellular junctions, glial cells develop special plasma membrane structural domains. Astrocytic plasma membranes often contain groups of plasmalemmal vesicles (caveolae), a distinctive feature of astrocytes in situ. Oligodendrocytes form flattened velate processes with cytoplasm restricted to finger-like channels resembling myelin lamellae in situ. Cultured astrocytes and oligodendrocytes develop the entire range of plasma membrane structural specializations seen in situ in the absence of the normal brain tissue framework. Thus, primary glial cell cultures allow experimental study of many glial cell properties, including their plasma membrane specializations.' }
+  let(:abstract_title) { 'Cell-cell junctional interactions and characteristic plasma membrane features of cultured rat glial cells.' }
+  let!(:ab) { FactoryGirl.create(:abstract, abstract: abstract_text, title: abstract_title) }
+  let!(:aw) { FactoryGirl.create(:proposal, institution_award_number: 'institution_award_number') }
+  let!(:st) { FactoryGirl.create(:study, irb_study_number: 'irb_study_number') }
+
+  before do
+    # set up nested set relationship
+    # cf. CleanUpOrganizationData in lib/organization_utilities.rb
+    dept1.parent_id = school.id
+    dept1.move_to_child_of school
+    dept1.save!
+
+    dept2.parent_id = school.id
+    dept2.move_to_child_of school
+    dept2.save!
+
+    # set up publication associations
+    FactoryGirl.create(:investigator_abstract, investigator: pi, abstract: ab, is_valid: true)
+    FactoryGirl.create(:investigator_abstract, investigator: co, abstract: ab, is_valid: true)
+
+    # cf. pubmed_utilities to see what these do
+    UpdateInvestigatorCitationInformation(pi)
+    UpdateInvestigatorCitationInformation(co)
+    BuildInvestigatorColleague(pi, co, false)
+
+    # set up award associations
+    FactoryGirl.create(:investigator_proposal, investigator: pi, proposal: aw, role: 'PD/PI', is_main_pi: true)
+
+    # set up study associations
+    FactoryGirl.create(:investigator_study, investigator: pi, study: st, role: 'PI')
+
+    # create the organization abstract records
+    [dept1, dept2].each do |unit|
+      unit_id = unit.id
+      investigators = OrganizationalUnit.find(unit_id).primary_faculty + OrganizationalUnit.find(unit_id).associated_faculty
+      abstracts = Abstract.all_investigator_publications(investigators.map(&:id)).uniq
+      abstracts.each { |abstract| UpdateOrganizationAbstract(unit_id, abstract.id) }
+    end
+  end
+
+  describe '.d3_master_investigator_graph' do
+    it 'outputs an array' do
+      graph = d3_master_investigator_graph(pi)
+      graph.size.should eq 2
+      assert_investigator_graph(graph[0], pi, co)
+      assert_investigator_graph(graph[1], co, pi)
+    end
+  end
+
+  describe '.d3_all_units_graph' do
+    it 'outputs an array of hashes of both org units and their investigators' do
+      units = school.descendants
+      units.size.should eq 2
+
+      graph = d3_all_units_graph(units)
+      graph.size.should eq 4 # 2 departments and 2 PIs
+
+      assert_department_graph(graph[0], dept1)
+      assert_investigator_graph(graph[1], pi, co, true)
+      assert_department_graph(graph[2], dept2)
+      assert_investigator_graph(graph[3], co, pi, true)
+    end
+  end
+
+  describe '.d3_all_investigators_graph' do
+    context 'for the whole school' do
+      it 'outputs an array of /all/ investigators (i.e. all departments)' do
+        graph = d3_all_investigators_graph(school)
+        graph.size.should eq 2
+        assert_investigator_graph(graph[0], co, pi)
+        assert_investigator_graph(graph[1], pi, co)
+      end
+    end
+    context 'per department' do
+      it 'outputs an array of all investigators for the department' do
+        [
+          [dept1, pi],
+          [dept2, co]
+        ].each do |dept, i|
+          graph = d3_all_investigators_graph(dept)
+          graph.size.should eq 1
+          assert_investigator_graph(graph.first, i, nil)
+        end
+      end
+    end
+  end
+
+  describe '.d3_investigator_wordle_data' do
+
+    before do
+      # setup the data like in rake task setup:wordle
+      WordFrequency.save_abstract_frequency_map
+      WordFrequency.save_investigator_frequency_map
+    end
+
+    it 'gathers words as an Array of Hashes' do
+      words = WordFrequency.investigator_wordle_data(pi)
+      words = WordFrequency.wordle_distribution(words)
+      words = words.flatten
+      words.size.should eq 150
+      words.each do |w|
+        [:word, :frequency, :the_type].each do |key|
+          w.should have_key(key)
+          case key
+          when :word
+            p w[key]
+          when :frequency
+            w[key].should eq 1 # as there is only one Abstract in the setup data
+          when :the_type
+            w[key].should eq 'Abstract'
+          end
+        end
+      end
+    end
+  end
+
+  ##
+  # Assert that the given Hash (graph) has the appropriate
+  # :name, :size, and :imports values for the given OrganzationalUnit
+  # @param Hash graph
+  # @param OrganizationalUnit ou
+  def assert_department_graph(graph, ou)
+    graph[:name].should eq ou.name
+    graph[:size].should eq OrganizationAbstract.where(organizational_unit_id: ou.id).count
+    graph[:imports].should eq ['']
+  end
+
+  ##
+  # Assert that the given Hash (graph) has the appropriate
+  # :name, :size, and :imports values for the given pi (i)
+  #
+  # :name is the matrix key for the chord diagram
+  # :size is the number of publications for the pi
+  # :imports is an Array of co-author :name values (either ou_name or co_author.last_name)
+  #
+  # @param Hash graph
+  # @param Investigator i - the investigator in question
+  # @param Investigaror i2 - co_author of the Investigator i (setup sets just one co-author)
+  # @param department_graph - default false - flag for matrix key
+  def assert_investigator_graph(graph, i, i2, department_graph = false)
+    name = department_graph ? ou_name(i) : i.last_name
+    graph[:name].should eq name
+    graph[:size].should eq pi.abstracts.count # probably should be scoped to the org unit
+    graph[:imports].should eq imports(department_graph, i2) # co-authorship associations
+  end
+
+  ##
+  # cf. the name function in /app/views/cytoscape/chord.html.erb
+  #   // Returns the Flare package name for the given class name.
+  #   // We can make it program name.investigator_username
+  # so this is the matrix key for the Organizational Unit chord diagram
+  def ou_name(i)
+    "#{i.memberships.first.name}.#{i.username}"
+  end
+
+  ##
+  # department matrix_key cf. ou_name or Investigator.last_name
+  def matrix_key(department_graph, i)
+    department_graph ? ou_name(i) : i.try(:last_name)
+  end
+
+  ##
+  # Handles case when i is nil
+  # otherwise creates a single element array of the
+  # investigator (either the ou_name or the pi last_name)
+  # @see matrix_key
+  # @return [Array<String>]
+  def imports(department_graph, i = nil)
+    [matrix_key(department_graph, i)].compact
+  end
+
+end
